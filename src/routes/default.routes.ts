6f65252eaceb52d6a527cab8fd02d4eb0b26e4eb
jpb <jpb.06@outlook.com>
2019-03-31 15:41:11 +0200

    Express routes

---
+++ src/routes/default.routes.ts
@@ -0,0 +1,24 @@
+﻿import { Express, Request, Response } from "express-serve-static-core";
+import { BusinessError } from "../business/errors/business.error";
+import { UsersManager } from "../business/users.manager";
+
+export function mapDefaultRoutes(
+    app: Express
+) {
+
+    // Root
+    app.get('/', (
+        req: Request,
+        res: Response
+    ) => {
+        res.send('Nothing to see here. See /api');
+    });
+
+    // Api
+    app.get('/api', (
+        req: Request,
+        res: Response
+    ) => {
+        res.send('Api root');
+    });
+}
---
+++ src/routes/security.routes.ts
@@ -0,0 +1,68 @@
+﻿import { Express, Request, Response } from "express-serve-static-core";
+import * as jwt from 'jsonwebtoken';
+import * as moment from 'moment';
+import { VaultService } from "rsa-vault";
+
+import { UsersManager } from "../business/users.manager";
+import { BusinessError } from "../business/errors/business.error";
+import * as CryptoUtil from './../business/security/crypto.util';
+
+import { ApplicationKeys } from "rsa-vault/typings/types.export";
+import { User } from "../dal/types/complex.types";
+
+const sessionDuration = 1200;
+
+export function mapSecurityRoutes(
+    app: Express
+) {
+
+    app.post('/api/login', async (
+        req: Request,
+        res: Response
+    ) => {
+        try {
+
+            if (!req.validateLogin()) {
+                return res.sendGenericError(400, 'Expecting identifiers');
+            }
+
+            let user: User = await UsersManager.getByEmail(req.body.login);
+
+            let isPasswordValid = await CryptoUtil.verify(req.body.password, user.hashedPassword);
+            if (isPasswordValid) {
+                let keys: ApplicationKeys = await VaultService.GetKeyPair('frontend-api-boilerplate');
+
+                const jwtBearerToken = jwt.sign({
+                    sub: user.email
+                }, keys.privateKey, {
+                    algorithm: 'RS256',
+                    expiresIn: sessionDuration
+                });
+
+                let expirationDate = moment().add(sessionDuration, 'seconds');
+
+                return res.status(200).json({
+                    status: 200,
+                    token: jwtBearerToken,
+                    user: {
+                        lastname: user.lastname,
+                        firstname: user.firstname,
+                        roles: user.roles,
+                    },
+                    expirationDate: JSON.stringify(expirationDate)
+                });
+            } else {
+                return res.status(401).json({
+                    status: 401,
+                    data: null
+                });
+            }
+        } catch (error) {
+            if (error instanceof BusinessError) {
+                return res.sendBusinessError(401, error.message, error.errorCode);
+            } else {
+                return res.sendGenericError(500, error.message);
+            }
+        }
+    });
+}

0c54931d4893ee7844942927e6664034736f4a15
jpb <jpb.06@outlook.com>
2019-03-31 15:41:00 +0200

    Business layer

---
+++ src/business/users.manager.ts
@@ -0,0 +1,86 @@
+﻿import { BusinessError, ErrorCode } from "./errors/business.error";
+import { UsersStore } from "../dal/manipulation/mongodb/stores/users.store";
+import { RolesStore } from "../dal/manipulation/mongodb/stores/roles.store";
+import { ObjectId } from "bson";
+import { DbRole } from "../dal/types/persisted.types";
+import { User } from "../dal/types/complex.types";
+import * as CryptoUtil from './security/crypto.util';
+
+export abstract class UsersManager {
+
+    public static async create(
+        lastname: string,
+        firstname: string,
+        email: string,
+        password: string
+    ): Promise<boolean> {
+
+        // checking first if email is not already being used
+        let usersWithEmail = await UsersStore.getByEmail(email);
+        if (usersWithEmail.length > 0) {
+            throw new BusinessError(`The email ${email} is already used`, ErrorCode.EmailAlreadyExists);
+        }
+
+        // mapping roles: creating default user role if it doesn't exist
+        let role: DbRole;
+        let matchingUserRoles = await RolesStore.getByName('user');
+        if (matchingUserRoles.length === 0) {
+            role = {
+                _id: new ObjectId(),
+                name: 'user'
+            };
+            let result = await RolesStore.create(role);
+            if (result !== true) {
+                throw new BusinessError('Could not initialize default user role', ErrorCode.UnableToPersistRole);
+            }
+        } else {
+            role = matchingUserRoles[0];
+        }
+
+        let hashedPassword = await CryptoUtil.hash(password);
+
+        // persisting user
+        let result = await UsersStore.create({
+            _id: new ObjectId(),
+            roles: [role._id],
+
+            lastname: lastname,
+            firstname: firstname,
+            email: email,
+            hashedPassword: hashedPassword
+        });
+
+        if (result !== true) {
+            throw new BusinessError(`Could not create user ${email}`, ErrorCode.UnableToPersistUser);
+        }
+
+        return true;
+    }
+
+    public static async getByEmail(
+        email: string
+    ): Promise<User> {
+
+        let rawUsers = await UsersStore.getByEmail(email);
+
+        if (rawUsers.length === 0) {
+            throw new BusinessError(`No existing user for email ${email}`, ErrorCode.NoUserForEmail);
+        } else if (rawUsers.length > 1) {
+            throw new BusinessError(`Several users found with email ${email}`, ErrorCode.EmailIsNotUnique);
+        }
+
+        let user = rawUsers[0];
+        let rawRoles = await RolesStore.getByIds(user.roles);
+
+        return {
+            id: user._id,
+            roles: user.roles.map((id: ObjectId) => 
+                (<DbRole>rawRoles.find(el => el._id.equals(id))).name
+            ),
+            lastname: user.lastname,
+            firstname: user.firstname,
+            email: user.email,
+            hashedPassword: user.hashedPassword
+        };
+    }
+}
